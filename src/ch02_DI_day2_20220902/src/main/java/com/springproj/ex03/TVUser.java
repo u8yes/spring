007dfc355599e355676260308cb7660b1d79512c
u8yes <108450018+u8yes@users.noreply.github.com>
2022-09-02 18:31:20 +0900

    Day2_20220902_6

--- src/ch02_DI_day2_20220902/src/main/java/com/springproj/ex03/TVUser.java
+++ src/ch02_DI_day2_20220902/src/main/java/com/springproj/ex03/TVUser.java
@@ -1,19 +1,21 @@
 package com.springproj.ex03;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class TVUser {
 
 	public static void main(String[] args) {
 		// TV tv = new LgTV();
-		ApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
+		AbstractApplicationContext  factory = new GenericXmlApplicationContext("applicationContext.xml");
 		
-		TV tv = factory.getBean("tv");
+		TV tv = (TV)factory.getBean("tv");
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
 		tv.powerOff();
+		
+		factory.close();
 	}
 
 }
