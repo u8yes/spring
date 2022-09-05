6af536f21b0be148d1c5a73cc500fea59ddae3d6
u8yes <108450018+u8yes@users.noreply.github.com>
2022-09-05 17:29:52 +0900

    Day3_20220905_7

---
+++ src/ch03_Collection_day3_20220905/src/main/java/ex01/collection/list/CollectionBean.java
@@ -0,0 +1,19 @@
+package ex01.collection.list;
+
+import java.util.List;
+
+public class CollectionBean {
+	
+	private List<String> addressList;
+
+	public List<String> getAddressList() {
+		return addressList;
+	}
+
+	public void setAddressList(List<String> addressList) {
+		this.addressList = addressList;
+	}
+	
+	
+	
+}
