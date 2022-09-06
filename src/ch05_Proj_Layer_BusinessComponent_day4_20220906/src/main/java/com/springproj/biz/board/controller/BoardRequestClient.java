fd217ff313006e0d269614aa28ece70503fc2589
u8yes <108450018+u8yes@users.noreply.github.com>
2022-09-06 18:21:20 +0900

    Day4; 20220906_4

---
+++ src/ch05_Proj_Layer_BusinessComponent_day4_20220906/src/main/java/com/springproj/biz/board/controller/BoardRequestClient.java
@@ -0,0 +1,10 @@
+package com.springproj.biz.board.controller;
+
+public class BoardRequestClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/ch05_Proj_Layer_BusinessComponent_day4_20220906/src/main/java/com/springproj/biz/board/service/BoardServiceImpl.java
@@ -0,0 +1,37 @@
+package com.springproj.biz.board.service;
+
+import com.springproj.biz.board.BoardVO;
+
+public class BoardServiceImpl implements BoardService {
+
+	@Override
+	public void insertService(BoardVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getService(int seq) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getServiceList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateService(BoardVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteService(int seq) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
