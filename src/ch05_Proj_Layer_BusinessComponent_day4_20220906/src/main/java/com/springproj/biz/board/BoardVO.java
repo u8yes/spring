6541117e8e942c53c9f38ca22bbbffa0023a40ed
u8yes <108450018+u8yes@users.noreply.github.com>
2022-09-06 17:24:32 +0900

    Day4; 20220906_2

--- src/ch05_Proj_Layer_BusinessComponent_day4_20220906/src/main/java/com/springproj/biz/board/BoardVO.java
+++ src/ch05_Proj_Layer_BusinessComponent_day4_20220906/src/main/java/com/springproj/biz/board/BoardVO.java
@@ -1,5 +1,58 @@
 package com.springproj.biz.board;
 
-public class BoardVO {
+import java.sql.Date;
+
+public class BoardVO {	// javaBean
 	private int seq; 
+	private String title;
+	private String writer;
+	private String content;
+	private Date regdate;
+	private int cnt;
+	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
+	@Override
+	public String toString() {
+		String data = "BoardVO[seq=" + seq + ", title=" + title + ", writer=" + writer 
+				+ ", content=" + content + ", date=" + regdate + ", cnt=" + cnt + "]";
+		return data;
+	}
+	
+	
 }
