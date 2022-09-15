315f5e5c7b06b3441a1b74318f52276d7ab60f95
u8yes <108450018+u8yes@users.noreply.github.com>
2022-09-15 17:41:03 +0900

    Day9; 20220915_4

--- src/ch11_SpringMVC_Board_day8~9_20220914~5/src/main/java/com/springproj/biz/board/controller/DeleteBoardProcController.java
+++ src/ch11_SpringMVC_Board_day8~9_20220914~5/src/main/java/com/springproj/biz/board/controller/DeleteBoardProcController.java
@@ -6,11 +6,22 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import com.springproj.biz.board.vo.BoardVO;
+
 public class DeleteBoardProcController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("테스트");
+		
+		/*
+		 * String seq = request.getParameter("seq");
+		 * 
+		 * BoardVO board = boardService.getService(Integer.parseInt(seq)); // 형변환 String
+		 * -> Integer ModelAndView mav = new ModelAndView(); mav.addObject("board",
+		 * board); mav.setViewName("getBoard");
+		 */
+		
 		return null;
 	}
 
