598d926f87756717ccce73fd1d98affac7848f74
u8yes <108450018+u8yes@users.noreply.github.com>
2022-09-26 17:31:59 +0900

    Day16; 20220926_2

--- src/fatmily/src/main/webapp/sql/brand.sql
+++ src/fatmily/src/main/webapp/sql/brand.sql
@@ -1,15 +1,14 @@
 create table brand(
-		seq	number(5)	primary key,
-		title	varchar2(200),
-		writer	varchar2(200),
-		content	varchar2(2000),
-		font_name         varchar2(100),
-		color_name       varchar2(100),
-		brand_uploadFile	varchar2(100),
-		regdate	date	default	sysdate,
-		cnt	number(5)	default	0
+		b_no	number(5)	primary key,
+		b_corp	varchar2(200) unique,
+		b_corp_addr	varchar2(200),
+		b_desc	varchar2(2000),
+		b_font_name        varchar2(100),
+		b_color_name       varchar2(100),
+		b_image		varchar2(100),
+		b_regdate	date	default	sysdate
 );
 
-select * from brand order by seq desc;
+select * from brand order by b_no desc;
 
 drop table brand;
